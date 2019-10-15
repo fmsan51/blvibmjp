fcac24dbb74ad819f588d377429e881560f52904
Y Fujimoto <fmsan51@gmail.com>
2019-10-15 21:11:12 +0900

    WIP

--- tests/testthat/test-change_area.R
+++ tests/testthat/test-change_area.R
@@ -10,13 +10,14 @@ test_that("condition works", {
                       "pregnancty",
                       "dry",
                       "dim > 100",
-                      NA,
-                      "age > 20 & parity == 3")
+                      "age > 20 & parity == 3",
+                      NA)
   area[, `:=`(area_id = 1:8,
               area_type = "free",
               capacity = Inf,
               next_area = 2:9,
               priority = 1)]
+  area <- setup_area_table(area)
   cows <- a_new_calf[rep(1, 8 * 2), ]
   cows$area_id <- rep(1:8, each = 2)
   cows[c(1:2), age := c(21, 20)]
@@ -24,10 +25,17 @@ test_that("condition works", {
   day <- 10
   cows[c(5:6), `:=`(date_last_delivery = c(day, day - 1),
                     i_month = day)]
-  cows[c(7:8), date_got_pregnant := c(day, day - 1)]
-  cows[c(9:10), stage := c("dry", "milking")]
-  ceows$age[c(1:2, 15:16)] <- c(21, 20, 21, 21)
-  cows$parity[c(3:4, 15:16)] <- c()
+  cows[c(7:8), `:=`(date_got_pregnant = c(day, day - 1),
+                    i_month = day)]
+  cows[c(9:10), `:=`(date_dried = c(day, day - 1),
+                     i_month = day)]
+  cows[c(11:12), `:=`(date_delivered = c(day - 4, day),
+                      i_month = day)]
+  cows[c(13:14), `:=`(age = c(21, 20),
+                      parity = c(3, 3))]
+  cows <- change_area(cows, area)
+
+
 
 
 })
