32f3883c0689502cddab722984da3b35a304a117
Y Fujimoto <fmsan51@gmail.com>
2020-04-03 13:49:14 +0900

    Remove: Remove update_if_not_null()

--- R/util.R
+++ R/util.R
@@ -1,31 +1,3 @@
-#' Update a list if an input is not `NULL`
-#'
-#' Overwrite components of a list if inputs are not `NULL`.
-#'
-#' @param list A list to be updated.
-#' @param ... Inputs to update.
-#'
-#' @return A list whose components which has same names with non-`NULL` inputs are overwritten.
-#'
-#' @examples
-#' a <- "A"
-#' b <- NULL
-#' l <- list(a = "a", b = "B", c = 3)
-#' \dontrun{
-#' update_if_input_not_null(l, a, b)  # list(a = "A", b = "B", c = 3)
-#' }
-update_if_input_not_null <- function(list, ...) {
-  input_list <- as.list(...)
-  input_name <- as.character(ensyms(...))
-  for (i in input_name) {
-    if (!is.null(input_list[[i]])) {
-      list[[i]] <- input_list[[i]]
-    }
-  }
-}
-# TODO:いらんかも
-
-
 #' Convert non-integer to integer
 #'
 #' Convert non-integer to integer. \cr
