ef24cfe91b176daf528345e4f4056496267b0dc9
Y Fujimoto <fmsan51@gmail.com>
2020-04-05 18:46:55 +0900

    Fix: Add packages to Imports

--- R/blvimbjp-package.R
+++ R/blvimbjp-package.R
@@ -6,9 +6,9 @@
 #' @importFrom data.table as.data.table CJ copy data.table dcast.data.table fifelse fcoalesce fread fwrite melt.data.table rbindlist shift := .N .SD
 #' @importFrom dplyr n_distinct
 #' @importFrom forcats fct_other
-#' @importFrom glue glue
 #' @importFrom ggplot2 aes element_blank element_line geom_area geom_point ggplot labs scale_fill_manual scale_color_manual scale_x_continuous theme theme_bw xlab xlim ylab ylim
 #' @importFrom ggthemes colorblind_pal
+#' @importFrom glue glue
 #' @importFrom lubridate day dmy dym interval mdy myd today tz ydm ymd
 #' @importFrom purrr flatten_dbl
 #' @importFrom readxl read_excel
