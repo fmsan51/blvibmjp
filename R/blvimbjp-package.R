e66c8cc9e39a37437b58aa92e73294751bd61036
Y Fujimoto <fmsan51@gmail.com>
2020-04-08 16:29:16 +0900

    Update: Import data.table::setorder()

--- R/blvimbjp-package.R
+++ R/blvimbjp-package.R
@@ -3,7 +3,7 @@
 #' This package provides functions to construct a individual based model of simulation of Bovine Leukemia virus in a tipical Japanese dairy herd.
 #'
 #' @importFrom cellranger cell_limits
-#' @importFrom data.table as.data.table CJ copy data.table dcast.data.table fifelse fcoalesce fread fwrite melt.data.table rbindlist shift := .N .SD
+#' @importFrom data.table as.data.table CJ copy data.table dcast.data.table fifelse fcoalesce fread fwrite melt.data.table rbindlist setorder shift := .N .SD
 #' @importFrom dplyr n_distinct
 #' @importFrom forcats fct_other
 #' @importFrom ggplot2 aes element_blank element_line geom_area geom_point ggplot labs scale_fill_manual scale_color_manual scale_x_continuous theme theme_bw xlab xlim ylab ylim
